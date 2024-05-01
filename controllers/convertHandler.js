3d9690917d2648c571448609eeab8edd6b6029fd
brennanting <164741100+brennanting@users.noreply.github.com>
2024-05-01 12:52:53 +0000

    feat: added tests and fixed convert handler code

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -4,86 +4,92 @@ const miToKm = 1.60934;
 
 const units = [
   {
-    symbol: 'gal', 
-    name: 'gallons',
-    returnSymbol: 'L',
-    convFactor: galToL
+    symbol: "gal",
+    name: "gallons",
+    returnSymbol: "L",
+    convFactor: galToL,
   },
   {
-    symbol: 'L',
-    name: 'liters',
-    returnSymbol: 'gal',
-    convFactor: 1/galToL
+    symbol: "L",
+    name: "liters",
+    returnSymbol: "gal",
+    convFactor: 1 / galToL,
   },
   {
-    symbol: 'lbs',
-    name: 'pounds',
-    returnSymbol: 'kg',
-    convFactor: lbsToKg
+    symbol: "lbs",
+    name: "pounds",
+    returnSymbol: "kg",
+    convFactor: lbsToKg,
   },
   {
-    symbol: 'kg',
-    name: 'kilograms',
-    returnSymbol: 'lbs',
-    convFactor: 1/lbsToKg
+    symbol: "kg",
+    name: "kilograms",
+    returnSymbol: "lbs",
+    convFactor: 1 / lbsToKg,
   },
   {
-    symbol: 'mi',
-    name: 'miles',
-    returnSymbol: 'km',
-    convFactor: miToKm
+    symbol: "mi",
+    name: "miles",
+    returnSymbol: "km",
+    convFactor: miToKm,
   },
   {
-    symbol: 'km',
-    name: 'kilometers',
-    returnSymbol: 'mi',
-    convFactor: 1/miToKm
-  }
-]
+    symbol: "km",
+    name: "kilometers",
+    returnSymbol: "mi",
+    convFactor: 1 / miToKm,
+  },
+];
 
 function ConvertHandler() {
   this.getNum = function (input) {
     let qtyString;
     let regex = /[a-zA-Z]/;
     // finds the first time a letter appears in the input
-    index = input.search(regex)
+    index = input.search(regex);
 
     // if first character is a letter
     if (index == 0) {
       // set quantity to default of 1
-      qtyString = '1';
+      qtyString = "1";
 
-    // otherwise if there is a non-letter character before the first letter
-    } else if (index > 0 ) {
+      // otherwise if there is a non-letter character before the first letter
+    } else if (index > 0) {
       // set quantity to the string before the first letter
       qtyString = input.slice(0, index);
 
-    // if index is less than 0 ie letter not found in the input
+      // if index is less than 0 ie letter not found in the input
     } else {
       // the whole input is the quantity
-      qtyString = input
+      qtyString = input;
     }
 
-    let slashIndex = qtyString.search('/')
+    let slashIndex = qtyString.search("/");
 
     // if there is a slash in the input
     if (slashIndex > 0) {
       let numerator = qtyString.slice(0, slashIndex);
       let denominator = qtyString.slice(slashIndex + 1, qtyString.length);
-      if (isNaN(numerator) || isNaN(denominator) || !numerator || ! denominator || denominator == 0) {
-        return;
+      if (
+        isNaN(numerator) ||
+        isNaN(denominator) ||
+        !numerator ||
+        !denominator ||
+        denominator == 0
+      ) {
+        return undefined;
       } else {
         let divResult = numerator / denominator;
         return divResult;
       }
-    // if no slash in input
+      // if no slash in input
     } else {
       //if NaN or empty
       if (!qtyString || isNaN(qtyString)) {
         // return invalid number
-        return;
-        
-      // else
+        return undefined;
+
+        // else
       } else {
         // return the result
         return Number(qtyString);
@@ -92,43 +98,44 @@ function ConvertHandler() {
   };
 
   this.getUnit = function (input) {
-    let unitString;
     let regex = /[a-zA-Z]/;
-    index = input.search(regex);
+    let index = input.search(regex);
     if (index < 0) {
-      return;
+      return undefined;
     }
-    unitString = input.slice(index, input.length);
-    return this.spellOutUnit(unitString);
+    let unitString = input.slice(index, input.length);
+    let unitIndex = units.findIndex(
+      (listedunit) =>
+        unitString.toLowerCase() == listedunit.symbol.toLowerCase(),
+    );
+    if (unitIndex == -1) {
+      return undefined;
+    } else {
+      return units[unitIndex].symbol;
+    }
   };
 
   this.getReturnUnit = function (initUnit) {
-    let index = units.findIndex(unit => unit.symbol == initUnit);
+    let index = units.findIndex((unit) => unit.symbol == initUnit);
     let returnUnit = units[index].returnSymbol;
     return returnUnit;
   };
 
   this.spellOutUnit = function (unit) {
-    let index = units.findIndex(listedunit => 
-        unit.toLowerCase() == listedunit.symbol.toLowerCase()
-      );
-    if (index == -1) {
-      return;
-    } else {
-      return units[index].symbol;
-    }
+    let index = units.findIndex((listedUnit) => unit == listedUnit.symbol);
+    return units[index].name;
   };
 
   this.convert = function (initNum, initUnit) {
-    
-    let index = units.findIndex(unit => unit.symbol == initUnit);
+    let index = units.findIndex((unit) => unit.symbol == initUnit);
     let result = initNum * units[index].convFactor;
-    let roundedResult = Math.round(result * 100000)/100000
+    let roundedResult = Math.round(result * 100000) / 100000;
     return roundedResult;
   };
 
   this.getString = function (initNum, initUnit, returnNum, returnUnit) {
-    let result = initNum + ' ' + initUnit + ' converts to ' + returnNum + ' ' + returnUnit;
+    let result =
+      initNum + " " + this.spellOutUnit(initUnit) + " converts to " + returnNum + " " + this.spellOutUnit(returnUnit);
     return result;
   };
 }
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,15 +6,33 @@ let convertHandler = new ConvertHandler();
 
 suite('Unit Tests', function(){
   // convertHandler should correctly read a whole number input.
+  test('Testing valid whole number input', () => {assert.strictEqual(convertHandler.getNum('5'), 5, 'Correctly read whole number input')})
   // convertHandler should correctly read a decimal number input.
+  test('Testing valid decimal number input', () => {assert.strictEqual(convertHandler.getNum('0.25'), 0.25, 'Correctly read decimal number input')})
   // convertHandler should correctly read a fractional input.
+  test('Testing valid fractional input', () => {assert.strictEqual(convertHandler.getNum('3/2'), 1.5, 'Correctly read fractional input')})
   // convertHandler should correctly read a fractional input with a decimal.
+  test('Testing valid fractional input with decimal', () => {assert.strictEqual(convertHandler.getNum('0.66/3'), 0.22, 'Correctly read fractional input with decimal')})
   // convertHandler should correctly return an error on a double-fraction (i.e. 3/2/3).
+  test('Testing invalid double-fraction input', () => {assert.strictEqual(convertHandler.getNum('3/2/3'), undefined, 'Correctly return error on double-fraction input')})
   // convertHandler should correctly default to a numerical input of 1 when no numerical input is provided.
+  test('Testing no numerical input', () => {assert.strictEqual(convertHandler.getNum('km'), 1, 'Correctly return 1 when no numerical input given')})
   // convertHandler should correctly read each valid input unit.
+  test('Testing valid input unit', () => {
+    assert.strictEqual(convertHandler.getUnit('km'), 'km', 'Correctly return valid input unit')
+  })
   // convertHandler should correctly return an error for an invalid input unit.
+  test('Testing invalid input unit', () => {
+    assert.strictEqual(convertHandler.getUnit('oz'), undefined, 'Correctly return error for invalid input unit')
+  })
   // convertHandler should return the correct return unit for each valid input unit.
+  test('Testing return unit for valid input unit', () => {
+    assert.strictEqual(convertHandler.getReturnUnit('lbs'), 'kg', 'Correctly return return unit for valid input unit')
+  })
   // convertHandler should correctly return the spelled-out string unit for each valid input unit.
+  test('Testing spelled out string unit for valid input unit', () => {
+    assert.strictEqual(convertHandler.spellOutUnit('lbs'), 'pounds', 'Correctly return spelled-out string unit for valid input unit')
+  })
   // convertHandler should correctly convert gal to L.
   // convertHandler should correctly convert L to gal.
   // convertHandler should correctly convert mi to km.
